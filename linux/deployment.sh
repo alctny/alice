7153a1b4fcc34b7bb179eded1ec085e1f7e4fd20
尸府主任 <gfiszhang@gmail.com>
2022-12-29 14:38:41 +0800

    - 添加家目录默认文件夹自动转化为小写脚本 - 添加 fcitx5-rime 自动安装配置脚本 - 添加部分 fcitx5 UI 配置 - 添加部分待完成脚本

---
+++ linux/deployment.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+cat inputrc >> ~/.inputrc
+cat vim >> ~/.vimrc
+cat bashrc >> ~/.bashrc
+
+bash home_dir_to_lower.sh
---
+++ linux/fedora/ibus-chinese-input.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+sudo dnf install ibus ibus-rime
+cat <<EOF >>~/.bashrc
+export GTK_IM_MODULE=ibus
+export XMODIFIERS=@im=ibus
+export QT_IM_MODULE=ibus
+ibus-daemon -x -d
+EOF
+
+if [ ! -d ~/.config/ibus/rime ]; then
+  mkdir -p ~/.config/ibus/rime
+fi
+cp -r ../rime/* ~/.config/ibus/rime/
