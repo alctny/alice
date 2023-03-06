21b3c5aea33200c2267a887394b773634020eabf
尸府主任 <gfiszhang@gmail.com>
2023-03-06 16:37:13 +0800

    1. 删除不必要脚本和配置 2. 删除资源文件

--- shell/linux/fcitx5-input.sh
+++ shell/linux/fcitx5-input.sh
@@ -1,14 +1,11 @@
 #! /bin/bash
 
 # base package
-sudo dnf install -y fcitx5                  # fcitx service
-sudo dnf install -y fcitx5-gtk              # support for gtk program
-sudo dnf install -y fcitx5-qt               # support for qt program
-sudo dnf install -y fcitx5-rime             # rime input
-sudo dnf install -y fcitx5-configtool        # config by gui tool
-# sudo dnf install -y kcm-fcitx5              # fcitx5-configtool but kde version
-# sudo dnf install -y fcitx5-chinese-addons   # fcitx chinese input method addons
-# sudo dnf install -y fcitx5-table-extra      # i forget
+sudo dnf install -y fcitx5              # fcitx service
+sudo dnf install -y fcitx5-gtk          # support for gtk program
+sudo dnf install -y fcitx5-qt           # support for qt program
+sudo dnf install -y fcitx5-rime         # rime input
+sudo dnf install -y fcitx5-configtool   # config by gui tool
 
 # set input evnrionment
 cat << EOF >> ~/.bashrc
@@ -17,15 +14,6 @@ export GTK_IM_MODULE=fcitx5
 export QT_IM_MODULE=fcitx5
 export XMODIFIERS=@im=fcitx5
 EOF
-
-# autostart
-# if [ ! -d ~/.config/autostart ]; then
-#   mkdir ~/.config/autostart
-# fi
-# ln -s /usr/share/applications/org.fcitx.Fcitx5.desktop ~/.config/autostart/
-# cat << EOF >> ~/.i3/conf
-# exec --no-startup-id fcitx5
-# EOF
 
 # select and add chinese input method by self
 fcitx5-configtool
--- shell/linux/fedora-i3wm-init.sh
+++ shell/linux/fedora-i3wm-init.sh
@@ -3,10 +3,6 @@
 # 上海时区
 timedatectl set-timezone Asia/Shanghai
 
-# 使用清华源
-
-SHELL_FOLDER=$(dirname $(readlink -f "$0"))
-
 # base tool
 sudo dnf install -y git
 sudo dnf install -y vim
@@ -17,37 +13,17 @@ sudo dnf install -y flameshot
 sudo dnf install -y thunderbird
 sudo dnf install -y chromium
 
-
 # program evnrionment
 sudo dnf group install 'C Development Tools and Libraries'
-# TODO: golang, rust, python, clang, cpp
-
-# coding program
-# TODO: VisualStudioCode
 
 # i3wm tool
 sudo dnf install -y w3m-image
 sudo dnf install -y rofi
 sudo dnf install -y polybar
 sudo dnf install -y picom
-sudo dnf install -y sddm
-sudo systemctl disable lightdm.service
-sudo systemctl enable sddm.service
 sudo dnf install -y nemo
 sudo dnf install -y feh
 sudo dnf install -y ranger
-
-# flatpak
-sudo flatpak install obs
-sudo flatpak install xmind
-sudo flatpak install steam
-
-# snap
-
-# isn't requair, just cool
-sudo dnf install -y neofetch
-
-neofetch
 
 echo "configure end!!!"
 
@@ -55,6 +31,4 @@ echo "configure end!!!"
 
 sudo dnf remove -y yelp
 sudo dnf remove -y mousepad
-sudo dnf remove -y lightdm
-sudo dnf remove -y i3status 
-sudo dnf remove -y firefox
+sudo dnf remove -y i3status
