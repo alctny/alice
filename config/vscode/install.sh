eeeaef711051db8d3cc28b2f8e32f304b1968392
尸府主任 <gfiszhang@gmail.com>
2023-03-06 13:41:17 +0800

    git 配置精简

--- config/vscode/install.sh
+++ config/vscode/install.sh
@@ -3,58 +3,45 @@
 # see mor info about code manager in terminal:
 # https://code.visualstudio.com/docs/editor/extension-marketplace#_command-line-extension-management
 
-code --extensions-dir ~/.local/share/vscode/extensions
-
 # general
-code --install-extension christian-kohler.path-intellisense
-code --install-extension Tyriar.sort-lines
+code --install-extension christian-kohler.path-intellisense # 路径自动补全
+code --install-extension Tyriar.sort-lines # 字符串排序
 # theme
-code --install-extension GitHub.github-vscode-theme
-code --install-extension illixion.vscode-vibrancy-continued
-# icon theme
-code --install-extension PKief.material-icon-theme
+code --install-extension GitHub.github-vscode-theme # GitHub 主题
+code --install-extension PKief.material-icon-theme # 图标主题
 # hight line
-code --install-extension cliffordfajardo.highlight-line-vscode
+code --install-extension cliffordfajardo.highlight-line-vscode # 高亮提示线
+
 # csv viewer
-code --install-extension GrapeCity.gc-excelviewer
-code --install-extension mechatroner.rainbow-csv
-# reg support
-code --install-extension ionutvmi.reg
+code --install-extension GrapeCity.gc-excelviewer # 表格样式查看 csv
+code --install-extension mechatroner.rainbow-csv # 彩虹色 csv
 
 # markdown
-code --install-extension yzhang.markdown-all-in-one
-code --install-extension bierner.markdown-footnotes
-code --install-extension DavidAnson.vscode-markdownlint
-code --install-extension yzane.markdown-pdf
-code --install-extension gera2ld.markmap-vscode
+code --install-extension yzhang.markdown-all-in-one # Markdown 工具包
+code --install-extension bierner.markdown-footnotes # Markdown 脚注
+code --install-extension DavidAnson.vscode-markdownlint # Markdown 语法检测
+code --install-extension yzane.markdown-pdf # 导出 Markdowon 为 PDF
+code --install-extension gera2ld.markmap-vscode # 用 Markdown 语法写思维导图
 
 # code tool
 # c/cpp
-code --install-extension ms-vscode.cpptools-extension-pack
-code --install-extension twxs.cmake
-code --install-extension ms-vscode.makefile-tools
+code --install-extension ms-vscode.cpptools-extension-pack # C/C++ 开发插件合集
+code --install-extension twxs.cmake # CMake 语言支持
+code --install-extension ms-vscode.makefile-tools # CMake 工程支持
 # golang
-code --install-extension golang.go
+code --install-extension golang.go # go 开发插件
 # python
-code --install-extension ms-python.python
+code --install-extension ms-python.python # python 开发插件合集
 # rust
-code --install-extension rust-lang.rust-analyzer
-code --install-extension tamasfe.even-better-toml
-code --install-extension vadimcn.vscode-lldb
-code --install-extension tauri-apps.tauri-vscode
+code --install-extension rust-lang.rust-analyzer # rust 语法检测
+code --install-extension tamasfe.even-better-toml # rust toml 支持
+code --install-extension vadimcn.vscode-lldb # rust 调试插件
 # fore-ent
-code --install-extension ritwickdey.LiveServer
-code --install-extension Vue.volar
-code --install-extension ecmel.vscode-html-css
-code --install-extension xabikos.JavaScriptSnippets
+code --install-extension ritwickdey.LiveServer # 即时预览
+code --install-extension ecmel.vscode-html-css # html/css 支持
+code --install-extension xabikos.JavaScriptSnippets # js 代码片段
 # shell
-code --install-extension foxundermoon.shell-format
+code --install-extension foxundermoon.shell-format # shell 格式化
 # version manager
-code --install-extension donjayamanne.githistory
-code --install-extension mhutchie.git-graph
-code --install-extension johnstoncode.svn-scm
-code --install-extension sumneko.lua
-
-if [ $(uname -a) = "Gnu/Linux" ]; then
-  cp -r settings.json keybindings.json snippets ~/.local/share/Code/User/
-fi
+code --install-extension donjayamanne.githistory # git 历史查看
+code --install-extension mhutchie.git-graph # 可视化 git
