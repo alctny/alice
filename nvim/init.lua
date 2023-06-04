11f172acfc2600db69547641950552d1dab866a3
尸府主任 <gfiszhang@gmail.com>
2023-06-05 00:12:47 +0800

    2023-06-05、

---
+++ nvim/init.lua
@@ -0,0 +1,8 @@
+require('basic')
+require('keybinding')
+require('plugin')
+require("colorscheme")
+require("plugin-config.nvim-tree")
+require("plugin-config.bufferline")
+require("plugin-config.lualine")
+
---
+++ nvim/lua/keybinding.lua
@@ -0,0 +1,86 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+local map = vim.api.nvim_set_keymap
+local opt = {noremap = true, silent = true}
+
+map('n', 's', '', opt)
+map('n', 'sv', ':vsp<CR>', opt)
+map('n', 'sh', ':sp<CR>', opt)
+map('n', 'sc', '<C-w>c', opt)
+map('n', 'so', '<C-w>o', opt)
+
+map('n', '<A-h>', '<C-w>h', opt)
+map('n', '<A-j>', '<C-w>j', opt)
+map('n', '<A-k>', '<C-w>k', opt)
+map('n', '<A-l>', '<C-w>l', opt)
+
+map('n', '<C-Left>', 'vertical resize -2<CR>', opt)
+map('n', '<C-Right>', 'vertical resize +2<CR>', opt)
+map('n', 's,', 'vertical resize -20<CR>', opt)
+map('n', 's.', 'vertical resize +20<CR>', opt)
+
+map('n', 'sj', 'resize +10<CR>', opt)
+map('n', 'sk', 'resize -10<CR>', opt)
+map('n', '<C-Down>', 'resize +2<CR>', opt)
+map('n', '<C-Up>', 'resize -2<CR>', opt)
+
+map('n', 's=', '<C-w>=', opt)
+
+-- 上下滚动浏览
+map("n", "<C-j>", "4j", opt)
+map("n", "<C-k>", "4k", opt)
+map("n", "<C-u>", "9k", opt)  -- ctrl u / ctrl + d  只移动9行，默认移动半屏
+map("n", "<C-d>", "9j", opt)
+
+-- 在visual 模式里粘贴不要复制
+map("v", "p", '"_dP', opt)
+
+-- 退出
+map("n", "q", ":q<CR>", opt)
+map("n", "qq", ":q!<CR>", opt)
+map("n", "Q", ":qa!<CR>", opt)
+
+-- insert 模式下，跳到行首行尾
+map("i", "<C-a>", "<ESC>I", opt)
+map("i", "<C-e>", "<ESC>A", opt)
+map("n", "<C-a>", "I<ESC>", opt)
+map("n", "<C-e>", "A<ESC>", opt)
+
+
+-- Terminal相关
+map("n", "<leader>t", ":sp | terminal<CR>", opt)
+map("n", "<leader>vt", ":vsp | terminal<CR>", opt)
+map("t", "<Esc>", "<C-\\><C-n>", opt)
+map("t", "<A-h>", [[ <C-\><C-N><C-w>h ]], opt)
+map("t", "<A-j>", [[ <C-\><C-N><C-w>j ]], opt)
+map("t", "<A-k>", [[ <C-\><C-N><C-w>k ]], opt)
+map("t", "<A-l>", [[ <C-\><C-N><C-w>l ]], opt)
+
+-- visual模式下缩进代码
+map("v", "<", "<gv", opt)
+map("v", ">", ">gv", opt)
+-- 上下移动选中文本
+map("v", "J", ":move '>+1<CR>gv-gv", opt)
+map("v", "K", ":move '<-2<CR>gv-gv", opt)
+
+-- nvim-tree
+-- alt + m 键打开关闭tree
+map("n", "<C-b>", ":NvimTreeToggle<CR>", opt)
+
+-- bufferline
+-- 左右Tab切换
+map("n", "<C-h>", ":BufferLineCyclePrev<CR>", opt)
+map("n", "<C-l>", ":BufferLineCycleNext<CR>", opt)
+-- 关闭
+--"moll/vim-bbye"
+map("n", "<C-w>", ":Bdelete!<CR>", opt)
+map("n", "<leader>bl", ":BufferLineCloseRight<CR>", opt)
+map("n", "<leader>bh", ":BufferLineCloseLeft<CR>", opt)
+map("n", "<leader>bc", ":BufferLinePickClose<CR>", opt)
+
+-- Telescope
+-- 查找文件
+map("n", "<C-p>", ":Telescope find_files<CR>", opt)
+-- 全局搜索
+map("n", "<C-f>", ":Telescope live_grep<CR>", opt)
+
