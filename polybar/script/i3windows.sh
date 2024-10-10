9b0a514ae573c49cc0ef8b395f3a6bed21e90bc1
尸府主任 <gfiszhang@gmail.com>
2024-10-10 22:58:53 +0800

    2024-10-10
    - polybar 自定义窗口名称

--- polybar/script/i3windows.sh
+++ polybar/script/i3windows.sh
@@ -1,46 +1,29 @@
 #! /bin/bash
  
-mode=1
-interval=0.2
+# FIXME: wmctl -l 在新开启的应用固定到非当前 workspace 时出现如下错误
+# X Error of failed request:  BadWindow (invalid Window parameter)
+# Major opcode of failed request:  20 (X_GetProperty)
+# Resource id in failed request:  0xa0000d
+# Serial number of failed request:  12
+# Current serial number in output stream:  12
+
+
+interval=0.3
 empt="Deus Vult"
+data_file="/home/$USER/.config/polybar/data/winname.txt"
 
-toggle() {
-  mode=$(((mode + 1) % 4))
-}
-
-trap "toggle" USR1
-
-# 把 xprop -id $id | grep _NET_WM_NAME 的内容直接传入可以获得一个更详细的窗口名称
-# e.g. "Visual Studio Code/" "Mozilla Firefox/"
-# 但由于单格窗口名称会更长，如果屏幕分辨率较低不建议使用
-prettier() {
-  wn=$@
-  if [[ "$wn" == *"- "* ]]; then
-    wn=$(echo $wn | awk -F'- ' '{print $NF}' | tr -d '" ')
-  elif [[ "$wn" == *"— "* ]]; then
-    wn=$(echo $wn | awk -F'— ' '{print $NF}' | tr -d '" ')
-  else
-    wn=$(echo $wn | awk -F'= ' '{print $NF}' | tr -d '" ')
-  fi
-  echo "${wn}/"
-}
-
-winall() {
-  ids=$(wmctrl -l -x | awk '{print $1}' | sed -E 's/^0x0+/0x/')
-  if [ -z "$ids" ]; then
-    echo $empt
-    exit
+# custom windows name
+wnamecustom() {
+  wname=$1
+  if [ ! -f $data_file ]; then
+    echo $wname
+    return
   fi
 
-  aid=$(xprop -root | grep '_NET_ACTIVE_WINDOW(WINDOW)' | awk '{print $NF}')
-  for id in $ids; do
-    wn=$(xprop -id $id WM_CLASS | awk -F'"' '{print $2}')
-    if [ "$id" = "$aid" ]; then
-      wn="*${wn}"
-    fi
-    echo -n "${wn} | "
-  done
-  echo
+  if [ "$(grep "$wname" "$data_file" | wc -l)" -gt 0 ];then
+    wname=$(grep "$wname" "$data_file" | awk -F',' '{print $2}')
+  fi
+  echo $wname
 }
 
 # 查看当前 workspace 所有窗口
@@ -49,12 +32,13 @@ winlis() {
   ids=$(wmctrl -l -x | awk  -v awi="$act_ws_id" '$2 == awi {print $1}' | sed -E 's/^0x0+/0x/')
   if [ -z "$ids" ]; then
     echo $empt
-    exit
+    return
   fi
 
   aid=$(xprop -root | grep '_NET_ACTIVE_WINDOW(WINDOW)' | awk '{print $NF}')
   for id in $ids; do
-    wn=$(xprop -id $id | grep CLASS | awk -F'"' '{print $2}')
+    wn=$(xprop -id $id WM_CLASS | awk -F'"' '{print $2}')
+    wn=$(wnamecustom "$wn")
     if [ "$id" = "$aid" ]; then
       wn="*${wn}"
     fi
@@ -63,31 +47,7 @@ winlis() {
   echo
 }
 
-# 只查看当前焦点窗口
-winact() {
-  id=$(xprop -root _NET_ACTIVE_WINDOW | awk '{print $NF}')
-  if [ "$id" = "0x0" ]; then
-    echo $empt
-    exit 0
-  fi
-
-  xprop -id $id WM_CLASS | awk -F'"' '{print $2}'
-}
-
 while true; do
-  case $mode in
-    0)
-      winact
-      ;;
-    1)
-      winlis
-      ;;
-    2)
-      winall
-      ;;
-    *)
-      echo $empt
-      ;;
-  esac
+  winlis
   sleep $interval
 done
