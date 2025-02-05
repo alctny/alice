bd3ca98b9e61928f0008e6243edb2ea911ec2925
尸府主任 <gfiszhang@gmail.com>
2025-02-05 11:06:21 +0800

    2025-02-05

--- nvim/lua/lazzy.lua
+++ nvim/lua/lazzy.lua
@@ -2,7 +2,7 @@ vim.keymap.set("n", "<leader>l", ":Lazy<CR>", { noremap = true, silent = true })
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.api.nvim_echo({"cloning lazy.nvim..."}, true, {})
+  -- vim.api.nvim_echo({"cloning lazy.nvim..."}, true, {})
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
