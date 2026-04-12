36688814075cbcaeaa5143c5ed0084d3211761c5
尸府主任 <gfiszhang@gmail.com>
2026-04-12 12:00:43 +0800

    04/12/2026
    - 刪除 dedup 腳本,使用 rmlint 更加專業
    - 調整部分腳本格式
    - 添加 RIME 在 Windows 上的主題樣式
    - 添加按照文件後綴刪除的腳本
    - 調整 VSCode 的 Copilot 聊天窗口按鈕位置

---
+++ firefox/user.js
@@ -0,0 +1,5 @@
+/* 當關閉最後一個標籤頁時，同時關閉該瀏覽器視窗 */
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("mozilla.widget.use-argb-visuals", boolean);
+user_pref("browser.translations.automaticallyPopup", true);
